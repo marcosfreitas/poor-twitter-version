7cbc1a5b8bf669847ff1e4f4f3ab1fc8d1424e98
Marcos Freitas <marcosvsfreitas@gmail.com>
2022-08-10 22:15:37 -0300

    feat(testing): removed user service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,13 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostModule } from '../post/post.module';
 import { UsersController } from './application/controllers/user.controller';
-import { UsersService } from './application/services/user.service';
 import { GetUserCommand } from './domain/commands/get-user.command';
 import { User } from './domain/contracts/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), forwardRef(() => PostModule)],
-  providers: [UsersService, GetUserCommand],
+  providers: [GetUserCommand],
   controllers: [UsersController],
   exports: [TypeOrmModule],
 })
